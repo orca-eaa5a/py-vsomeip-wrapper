37098709f16fca36ae0b48bfba2caf9d6ec7a75f
orca <orca@debian>
2024-03-04 12:49:00 +0900

    set vsomeip lib

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup, Extension
 vsomeipPy = Extension('vsomeip',
                     sources = ['vsomeip_python.cpp'],
                     libraries = ['vsomeip', 'vsomeip-cfg'],
-                    library_dirs = ['/usr/local/lib/'],
+                    library_dirs = ['/usr/local/lib/', '/home/orca/proj/vsomeip-3.4.9-r1/build'],
                     extra_compile_args=['-std=c++11'],
-                    runtime_library_dirs=['/usr/local/lib/'])
+                    runtime_library_dirs=['/usr/local/lib/', '/home/orca/proj/vsomeip-3.4.9-r1/build'],
+                    include_dirs=['/home/orca/proj/vsomeip-3.4.9-r1/interface'])
 
 setup (name = 'vsomeipPy',
        version = '1.0',
